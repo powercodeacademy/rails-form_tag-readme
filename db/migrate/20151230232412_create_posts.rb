888047da30e1b1f99b7c5aaeda76bf2b17b02331
Mike <mclancy96@gmail.com>
2025-08-19 14:11:22 -0400

    Fix migration

--- db/migrate/20151230232412_create_posts.rb
+++ db/migrate/20151230232412_create_posts.rb
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-class CreatePosts < ActiveRecord::Migration[5.2]
-=======
 class CreatePosts < ActiveRecord::Migration[7.1]
->>>>>>> rails-7.1
   def change
     create_table :posts do |t|
       t.string :title
